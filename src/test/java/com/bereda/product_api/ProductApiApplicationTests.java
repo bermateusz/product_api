40d7bb96eabf68f9696d91cfd09c4608a668f03c
bermateusz <beredamateusz@gmail.com>
2020-11-24 14:17:03 +0100

    first commit

---
+++ src/test/java/com/bereda/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bereda.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
